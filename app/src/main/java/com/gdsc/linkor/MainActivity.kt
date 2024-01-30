92de0559d270b42ef934c0a3135d34cde2291fbd
jhjalison01 <alison01@naver.com>
2024-01-31 00:48:59 +0900

    [Feat] #1 Add google sign in

--- app/src/main/java/com/gdsc/linkor/MainActivity.kt
+++ app/src/main/java/com/gdsc/linkor/MainActivity.kt
@@ -11,33 +11,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.gdsc.linkor.ui.theme.LinkorTheme
+import dagger.hilt.android.AndroidEntryPoint
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LinkorTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                //SignInScreen(signInViewModel = SignInViewModel())
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LinkorTheme {
-        Greeting("Android")
     }
 }
