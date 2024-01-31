8efab2c3d21092839cc505e5529be6a7ee9092f2
jhjalison01 <alison01@naver.com>
2024-01-31 19:50:24 +0900

    [Chore] solve error

--- app/src/main/java/com/gdsc/linkor/ui/setting/Mode.kt
+++ app/src/main/java/com/gdsc/linkor/ui/setting/Mode.kt
@@ -28,7 +28,7 @@ import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.linkor.R
+import com.gdsc.linkor.R
 
 
 data class Mode(@StringRes val stringResourceId: Int)
