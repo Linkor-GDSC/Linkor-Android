e1978843575a39d093ef761a9bc27b3d9a836778
SeMin <114004065+semsemin@users.noreply.github.com>
2024-01-31 19:27:30 +0900

    detail

---
+++ app/src/main/java/com/gdsc/linkor/ui/setting/question/locationArray.kt
@@ -0,0 +1,9 @@
+package com.gdsc.linkor.setting.question
+
+var city = listOf(
+    "City","서울특별시", "경기도", "강원도","충청북도", "충청남도", "전라북도","전라남도", "경상북도", "경상남도","제주도"
+)
+
+var district = listOf(
+    ""
+)
