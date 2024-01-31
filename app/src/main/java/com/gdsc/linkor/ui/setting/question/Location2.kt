0c4ade9cf229633c2edaf9e79083b00e9e9a51fb
jhjalison01 <alison01@naver.com>
2024-01-31 19:50:24 +0900

    [Chore] solve error

--- app/src/main/java/com/gdsc/linkor/ui/setting/question/Location2.kt
+++ app/src/main/java/com/gdsc/linkor/ui/setting/question/Location2.kt
@@ -43,7 +43,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.linkor.R
+import com.gdsc.linkor.R
 import com.gdsc.linkor.setting.QuestionViewModel
 import com.gdsc.linkor.setting.QuestionWrapper
 
