281940f9e0a600155b7d7cb8cfb9a7d792e51918
jhjalison01 <alison01@naver.com>
2024-02-06 18:21:44 +0900

    [Refactor] Split component into different files

--- app/src/main/java/com/gdsc/linkor/ui/tutorlist/TutorDetailScreen.kt
+++ app/src/main/java/com/gdsc/linkor/ui/tutorlist/TutorDetailScreen.kt
@@ -1,6 +1,5 @@
 package com.gdsc.linkor.ui.tutorlist
 
-import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -17,8 +16,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
