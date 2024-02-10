f11d9034804bea0cafb43cd9ce0a01141805d5a5
jhjalison01 <alison01@naver.com>
2024-02-11 00:05:33 +0900

    [Feature] #13 Add Bottom Navigation

--- app/src/main/java/com/gdsc/linkor/Navigation.kt
+++ app/src/main/java/com/gdsc/linkor/Navigation.kt
@@ -1,5 +1,6 @@
 package com.gdsc.linkor
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
@@ -10,8 +11,11 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
 import com.gdsc.linkor.Destinations.QUESTION_RESULTS_ROUTE
 import com.gdsc.linkor.Destinations.QUESTION_ROUTE
+import com.gdsc.linkor.navigation.BottomNavItem
+import com.gdsc.linkor.navigation.Route
 import com.gdsc.linkor.setting.QuestionResultScreen
 import com.gdsc.linkor.setting.QuestionRoute
+import com.gdsc.linkor.ui.learning.LearningScreen
 import com.gdsc.linkor.ui.tutorlist.TutorDetailScreen
 import com.gdsc.linkor.ui.tutorlist.TutorListScreen
 
@@ -20,17 +24,17 @@ object Destinations{
     const val QUESTION_ROUTE = "question"
     const val QUESTION_RESULTS_ROUTE = "questionresults"
 }
-object Graph {
+/*object Graph {
     const val TUTOR = "tutor_graph"
-}
+}*/
 
 @Composable
 fun LinkorNavHost(){
 
     val navController: NavHostController = rememberNavController()
-    NavHost(
+    /*NavHost(
         navController = navController,
-        /*   첫화면  */
+        *//*   첫화면  *//*
         startDestination = QUESTION_ROUTE,
     ){
         composable(QUESTION_ROUTE){
@@ -41,8 +45,8 @@ fun LinkorNavHost(){
             )
         }
 
-        /*  done 클릭 이후 보여지는 화면 연결 > 나중에 수정하면 될듯..
-        *  > Question.kt 안의 QuestionResultScreen() 도 함께 수정해야함 */
+        *//*  done 클릭 이후 보여지는 화면 연결 > 나중에 수정하면 될듯..
+        *  > Question.kt 안의 QuestionResultScreen() 도 함께 수정해야함 *//*
 
         composable(QUESTION_RESULTS_ROUTE) {
             QuestionResultScreen {
@@ -50,16 +54,31 @@ fun LinkorNavHost(){
             }
         }
 
-    }
-
-    /*NavHost(navController, startDestination = Graph.TUTOR) {
-        tutorListGraph(navController)
     }*/
+
+    NavHost(navController = navController, startDestination = Route.TUTOR) {
+
+        try {
+            tutorListGraph(navController)
+        } catch (e:Exception){
+            Log.e("MYTAG","tutor list graph error",e)
+        }
+
+
+        composable(BottomNavItem.Learning.screenRoute) {
+            LearningScreen(navController = navController)
+        }
+        composable(BottomNavItem.Community.screenRoute) {
+        }
+        composable(BottomNavItem.MyPage.screenRoute) {
+
+        }
+    }
 }
 
 fun NavGraphBuilder.tutorListGraph(navController: NavController) {
-    navigation(startDestination = "tutor_list", route = Graph.TUTOR) {
-        composable("tutor_list") { TutorListScreen(navController)
+    navigation(startDestination = Route.TUTORLIST, route = Route.TUTOR) {
+        composable(Route.TUTORLIST) { TutorListScreen(navController)
         }
         composable("tutor_detail/{photoUrl}/{name}/{gender}/{locationGu}/{locationSido}/{time}/{tutoringMethod}/{introduction}")
         {
---
+++ app/src/main/java/com/gdsc/linkor/navigation/Route.kt
@@ -0,0 +1,12 @@
+package com.gdsc.linkor.navigation
+
+object Route {
+    const val TUTOR = "tutor_graph"
+    const val TUTORLIST = "tutor_list"
+
+    const val LEARNING = "learning"
+
+    const val COMMUNITY = "community"
+
+    const val MYPAGE = "mypage"
+}
